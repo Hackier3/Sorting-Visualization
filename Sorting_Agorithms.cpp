78042de0f41a9cdc721a955df80bf838b99d186d
Filip Klimek <FilipKlimek102@gmail.com>
2022-05-21 23:12:41 +0200

    dodanie funkcji sortujacej przez scalanie

--- Sorting_Agorithms.cpp
+++ Sorting_Agorithms.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 /////////////////////////////////////    ctr + k + d	znajdowanie sciezki, gra	http://lazyfoo.net/tutorials/SDL/01_hello_SDL/windows/msvc2019/index.php
-void scal(vector <int>& vec);
-void sortuj_scalajac(vector <int>&, int, int, int);
+void scal(vector <int>&, int, int, int);
+void sortuj_scalajac(vector <int>&, int, int);
 void sortuj_zliczajac(vector <int>&, int, int, int);
 void sortuj_wstawiajac(vector <int>&);
 void sortuj_wybierajac(vector <int>&);
@@ -18,21 +18,62 @@ int main()
 	vector <int> vec = { 6,5,4,3,2,1 };
 
 	drukuj(vec);
-	sortuj_babelkowo(vec);
+	sortuj_scalajac(vec, 0, vec.size() - 1);
 	drukuj(vec);
 
 	return 0;
 }
 
 ///////////////////////////////////////
-void sortuj_scalajac(vector <int>& vec, int lewa, int srodek, int prawa)
+void sortuj_scalajac(vector <int>& vec, int lewa, int prawa)
 {
-
+	if (lewa != prawa)
+	{
+		int srodek = (prawa + lewa) / 2;
+		sortuj_scalajac(vec, lewa, srodek);
+		sortuj_scalajac(vec, srodek + 1, prawa);
+		scal(vec, lewa, srodek, prawa);
+	}
 }
 
-void scal(vector <int>& vec)
+void scal(vector <int>& vec, int lewa, int srodek, int prawa)
 {
+	//deklaruje rozmiar każdego z podvectorow
+	int rozmL = srodek - lewa + 1;
+	int rozmP = prawa - srodek;
 
+	//deklaruje vectory pomocniecze dla lewej oraz prawej strony
+	vector <int> vecL(rozmL);
+	vector <int> vecP(rozmP);
+
+	//przypisuje wartosc vectorom pomocniczym
+	for (int i = 0; i < rozmL; i++)
+		vecL.at(i) = vec.at(i + lewa);
+
+	for (int i = 0; i < rozmP; i++)
+		vecP.at(i) = vec.at(i + srodek + 1);
+
+	int wskaznikL = 0;
+	int wskaznikP = 0;
+	int indexVec;
+	
+	for (indexVec = lewa; wskaznikL < rozmL && wskaznikP < rozmP; indexVec++)
+	{
+		if (vecL[wskaznikL] <= vecP[wskaznikP])
+			vec[indexVec] = vecL[wskaznikL++];
+		else
+			vec[indexVec] = vecP[wskaznikP++];
+	}
+
+	//Jeśli w tablicy tabL pozostały jeszcze jakieś elementy
+	//kopiujemy je
+	while (wskaznikL < rozmL)
+		vec[indexVec++] = vecL[wskaznikL++];
+
+	//Jeśli w tablicy tabR pozostały jeszcze jakieś elementy
+	//kopiujemy je
+	while (wskaznikP < rozmP)
+		vec[indexVec++] = vecP[wskaznikP++];
 }
 
 void sortuj_zliczajac(vector <int>& vec)
@@ -102,10 +143,10 @@ void sortuj_babelkowo(vector <int>& vec)
 
 	if (i != 1)
 	{
-		i--;	sortuj_babelkowo(vec);
-	
-}
-		
+		i--;
+		sortuj_babelkowo(vec);
+	}
+
 	if (i != vec.size())
 		i = vec.size();
 }
